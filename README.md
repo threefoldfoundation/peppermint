0b559c21fcd0aeab708a94e3821ab8e86a5d27f4
Scott Yeager <yeagersm@gmail.com>
2024-09-26 10:01:53 -0700

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Quickstart
+
+```
+git clone https://github.com/scottyeager/peppermint.git
+
+cd peppermint
+mkdir venv
+python3 -m venv venv
+pip install python-fasthtml requests
+
+python3 main.py
+```
+
+A link to visit the dev server will appear in your terminal.
---
+++ main.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import requests
+from fasthtml.common import *
+
+app, rt = fast_app(live=True)
+
+
+@rt("/")
+def get():
+    return Titled(
+        "Fetch Node Minting Receipts",
+        Form(hx_post="/submit", hx_target="#result", hx_trigger="submit")(
+            Input(type="int", id="node_id", placeholder=42),
+            Button("Go", type="submit"),
+        ),
+        Div(id="result"),
+    )
+
+
+@rt("/submit")
+def post(d: dict):
+    try:
+        node_id = d["node_id"]
+    except ValueError:
+        return "Node id should be numbers only"
+
+    receipts = requests.get(
+        f"https://alpha.minting.tfchain.grid.tf/api/v1/node/{node_id}"
+    ).json()
+    header = Tr(
+        Th(Strong("Period Start")),
+        Th(Strong("Period End")),
+        Th(Strong("Uptime")),
+        Th(Strong("TFT Minted")),
+    )
+    rows = [header]
+    for r in receipts:
+        if "Minting" in r["receipt"]:
+            r = r["receipt"]["Minting"]
+            uptime = round(r["measured_uptime"] / (30.45 * 24 * 60 * 60) * 100, 2)
+            rows.append(
+                Tr(
+                    Td(datetime.fromtimestamp(r["period"]["start"]).date()),
+                    Td(datetime.fromtimestamp(r["period"]["end"]).date()),
+                    Td(f"{uptime}%"),
+                    Td(r["reward"]["tft"] / 1e7),
+                )
+            )
+    return Table(*rows)
+
+
+serve()
