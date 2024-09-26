be7aa786aad8e8aec5d076290cf3e80dc475a2ea
Scott Yeager <yeagersm@gmail.com>
2024-09-26 15:41:27 -0700

    Add farm support

--- main.py
+++ main.py
@@ -1,27 +1,79 @@
 from datetime import datetime
 import requests
 from fasthtml.common import *
+import grid3.network
 
+mainnet = grid3.network.GridNetwork()
 app, rt = fast_app(live=True)
 
 
 @rt("/")
-def get():
+def get(id_input: int = None, select: str = "Node ID"):
+    if id_input:
+        result = render_receipts(id_input)
+    else:
+        result = None
+
     return Titled(
-        "Fetch Node Minting Receipts",
-        Form(hx_post="/submit", hx_target="#result", hx_trigger="submit")(
-            Input(type="int", id="node_id", placeholder=42),
-            Button("Go", type="submit"),
+        "Fetch Minting Receipts",
+        Form(
+            hx_get="/submit",
+            hx_target="#result",
+            hx_trigger="submit",
+            # hx_push_url="true",
+        )(
+            Div(
+                Div(
+                    Input(
+                        type="int",
+                        id="id_input",
+                        placeholder=42,
+                        value=id_input,
+                    ),
+                    style="display: inline-block",
+                ),
+                Div(
+                    Select(
+                        Option("Node ID"),
+                        Option("Farm ID"),
+                        id="select",
+                        value=select,
+                    ),
+                    style="display: inline-block",
+                ),
+                Div(
+                    Button("Go", type="submit"),
+                    style="display: inline-block",
+                ),
+            ),
+            # CheckboxX(id="fixups", label="Show fixups"),
         ),
-        Div(id="result"),
+        Br(),
+        Div(result, id="result"),
     )
 
 
 @rt("/submit")
-def post(d: dict):
-    try:
-        node_id = int(d["node_id"])
-    except ValueError:
+def get(id_input: int, select: str):
+    if select == "Node ID":
+        return render_receipts(id_input)
+    elif select == "Farm ID":
+        nodes = mainnet.graphql.nodes(["nodeID"], farmID_eq=id_input)
+        node_ids = sorted([node["nodeID"] for node in nodes])
+        response = []
+        for node in node_ids:
+            response.append(H2("Node " + str(node)))
+            response.append(render_receipts(node))
+    return response
+
+
+def render_receipts(node_id):
+    if node_id:
+        try:
+            node_id = int(node_id)
+        except ValueError:
+            return "Please enter a valid node id"
+    else:
         return "Please enter a valid node id"
 
     receipts = requests.get(
